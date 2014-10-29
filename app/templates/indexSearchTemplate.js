897ce5355f1371a66bcedbbb71ee171d169035cf
Jimmy Tsao <jimmy.tsao@gmail.com>
2014-10-29 18:32:54 -0500

    (update) Updated formatting of templates to use a string buffer for readability

--- app/templates/indexSearchTemplate.js
+++ app/templates/indexSearchTemplate.js
@@ -2,7 +2,11 @@
 
 (function(App, _){
   
-  var template = '<div><input type="text" class="searchInput"><button type="button">Search</button></div>';
+  var template = [
+    '<div>',
+      '<input type="text" class="searchInput">',
+      '<button type="button">Search</button>',
+    '</div>'].join('');
 
   App.templates.indexSearch = _.template(template);
 
